298b6878a6ecaa5373465c3a62b89c9772ce8fdf
Benjamin Zanini <benjamin.zanini@outlook.com>
2020-03-11 16:36:10 +0100

    Initialisation corrigee

--- src/Depart.java
+++ src/Depart.java
@@ -1,11 +1,7 @@
-import java.util.Random;
-
 public class Depart {
 	
 	int nbrJoueurs;
 	Sac sac;
-	Random rand_j1 = new Random();
-	Random rand_j2 = new Random();
 	Lettre[] player1 = new Lettre[7];
 	Lettre[] player2 = new Lettre[7];
 	
@@ -15,12 +11,18 @@ public class Depart {
 	}
 	
 	public void tirageAleatoire() {
+		int positionLettrePiocheJ1 = 0;
+		int positionLettrePiocheJ2 = 0;
+		double rand_j1 = Math.random();
+		double rand_j2 = Math.random();
 		for(int i=0; i<7; i++) {
-			rand_j1 = nextInt(this.sac.s_lettres.size());
-			player1[i] = rand_j1;
-			rand_j2 = nextInt(this.sac.s_lettres.size());
-			player2[i] = rand_j2;
-		}
+			positionLettrePiocheJ1 = (int)rand_j1 * this.sac.s_lettres.size();
+			player1[i] = this.sac.s_lettres.get(i);
+			this.sac.s_lettres.remove(positionLettrePiocheJ1);
+			positionLettrePiocheJ2 = (int)rand_j2 * this.sac.s_lettres.size();
+			player2[i] = this.sac.s_lettres.get(i);
+			this.sac.s_lettres.remove(positionLettrePiocheJ2);
+ 		}
 	}
 	
 	
