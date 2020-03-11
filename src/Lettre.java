ae256836a939431821c685e2b0d13dcec742141b
Hamdi Lazaar <hlazaar@pcalb-mm0000.univ-jfc.fr>
2020-03-11 12:18:01 +0100

    premiere version

---
+++ src/Lettre.java
@@ -0,0 +1,13 @@
+public class Lettre{
+	
+	public char ch;
+	public int pts;
+	//public int nbrs;
+	//public static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
+	
+	public Lettre(char c, int p) {
+		this.ch = c;
+		this.pts = p;
+	}
+	
+}
---
+++ src/Sac.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+
+public class Sac {
+	
+	public Object[] o = {}; 
+	public ArrayList<Lettre> s_lettres = new ArrayList<Lettre>();
+	
+	public Sac() {
+		create('a',1,9);
+		create('b',3,2);
+		create('c',3,2);
+		create('d',2,3);
+		create('e',1,15);
+		create('f',4,2);
+		create('g',2,2);
+		create('h',4,2);
+		create('i',1,8);
+		create('j',8,1);
+		create('k',10,1);
+		create('l',1,5);
+		create('m',2,3);
+		create('n',1,6);
+		create('o',1,6);
+		create('p',3,2);
+		create('q',8,1);
+		create('r',1,6);
+		create('s',1,6);
+		create('t',1,6);
+		create('u',1,6);
+		create('v',4,6);
+		create('w',10,1);
+		create('x',10,1);
+		create('y',10,1);
+		create('z',10,1);
+		create('*',0,2);
+		}
+	
+	public void create(char c,int pt , int nb){
+		for(int i = 0; i<nb; i++) {
+			Lettre ln = new Lettre(c,pt);
+			s_lettres.add(ln);
+		}
+	}
+}
