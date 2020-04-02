1df6d47d99b280ff92ada6c19ffb5d6e4ec57b86
Benjamin Zanini <benjamin.zanini@outlook.com>
2020-04-02 10:33:38 +0200

    Gestion des coordonnées

--- src/Test_javafx.java
+++ src/Test_javafx.java
@@ -14,6 +14,14 @@ import javafx.stage.Stage;
 
 public class Test_javafx extends Application {
 
+	//Dimensions de la fenêtre
+	static int MAP_WIDTH = 300;
+	static int MAP_HEIGHT = 250;
+	
+	//Dimensions de chaque Tile (Ici, nos cases du Scrabble)
+	static int TILE_WIDTH = 30;
+	static int TILE_HEIGHT = 30;
+	
     @Override
     public void start(Stage stage) {
 
@@ -24,7 +32,7 @@ public class Test_javafx extends Application {
 
         StackPane root = new StackPane();
 
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, MAP_WIDTH, MAP_HEIGHT);
 
         Label lbl = new Label("Simple JavaFX application.");
         lbl.setFont(Font.font("Serif", FontWeight.NORMAL, 20));
@@ -43,9 +51,31 @@ public class Test_javafx extends Application {
         	
         });
         
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent recup_coord) {
+				// TODO Auto-generated method stub
+				double x = recup_coord.getX();
+				double y = recup_coord.getY();
+				
+				int colonne = (int) (Math.floor(x / TILE_WIDTH) + 1);
+				int ligne = (int) (Math.floor(y / TILE_HEIGHT) + 1);
+				
+				if(colonne > 0 && colonne <= MAP_WIDTH && ligne > 0 && ligne <= MAP_HEIGHT) {
+					
+					//Là je pourrais récupérer la position dans la matrice ->  id = plateau[ligne][colonne]
+					System.out.println("ligne : " + ligne + " colonne : " + colonne);
+				}
+			}
+        	
+        });
+        
         lbl.setFocusTraversable(true); 
         lbl.setOnKeyPressed(keyEvent -> System.out.printf("Touche enfoncée : %s", keyEvent.getCode()).println());
         root.getChildren().setAll(lbl);
+        
+        
     }
     
     public void modif_du_titre(Label lb) {
