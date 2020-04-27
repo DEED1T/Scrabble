a0254b75e499cb89881179eaf900b4a7fa1dc466
Lazaar Hamdi <hamdilazaar.vp@gmail.com>
2020-04-28 01:27:51 +0200

    reprise du SCRABBLE

--- src/Modele.java
+++ src/Modele.java
@@ -8,6 +8,7 @@ public class Modele {
 	Sac s = new Sac();
 	ArrayList<Lettre> pieces = s.get();
 	ArrayList<Lettre> j1 = new ArrayList<Lettre>();
+	ArrayList<Lettre> mot_en_cours = new ArrayList<Lettre>();
 	Random r = new Random(10);
 	
 	private int pivot = 0; 
@@ -15,9 +16,25 @@ public class Modele {
 	public int vide_j1 = 7;
 	private int first_i,first_j;
 	private int longeur=0;
+	public int score = 0;
 	
 	static private int slots = 7;
-	
+	static private int mod_plateau[][] = { {6,0,0,3,0,0,0,6,0,0,0,3,0,0,6},
+											{0,5,0,0,0,4,0,0,0,4,0,0,0,5,0},
+											{0,0,5,0,0,0,3,0,3,0,0,0,5,0,0},
+											{3,0,0,5,0,0,0,3,0,0,0,5,0,0,3},
+											{0,0,0,0,5,0,0,0,0,0,5,0,0,0,0},
+											{0,4,0,0,0,4,0,0,0,4,0,0,0,4,0},
+											{0,0,3,0,0,0,3,0,3,0,0,0,3,0,0},
+											{6,0,0,3,0,0,0,7,0,0,0,3,0,0,6},
+											{0,0,3,0,0,0,3,0,3,0,0,0,3,0,0},
+											{0,4,0,0,0,4,0,0,0,4,0,0,0,4,0},
+											{0,0,0,0,5,0,0,0,0,0,5,0,0,0,0},
+											{3,0,0,5,0,0,0,3,0,0,0,5,0,0,3},
+											{0,0,5,0,0,0,3,0,3,0,0,0,5,0,0},
+											{0,5,0,0,0,4,0,0,0,4,0,0,0,5,0},
+											{6,0,0,3,0,0,0,6,0,0,0,3,0,0,6}};
+
 	private int plateau[][] = { {6,0,0,3,0,0,0,6,0,0,0,3,0,0,6},
 								{0,5,0,0,0,4,0,0,0,4,0,0,0,5,0},
 								{0,0,5,0,0,0,3,0,3,0,0,0,5,0,0},
@@ -55,7 +72,7 @@ public class Modele {
 	
 	public Modele() {}
 	
-	//--------------méthodes d'utilité-------------------------------------
+	//--------------mï¿½thodes d'utilitï¿½-------------------------------------
 	
 	public void print_plateau() {
 		System.out.println();
@@ -66,7 +83,18 @@ public class Modele {
 			System.out.println();
 		}
 	}
-	//--------------fonctions de suivis/vérification----------------------
+	
+	public void print_charplat() {
+		System.out.println();
+		for(int i=0;i<plateau.length;i++) {
+			for(int j=0;j<plateau.length;j++) {
+				System.out.print(plat_char[j][i]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//--------------fonctions de suivis/vï¿½rification----------------------
 	
 	public char get_char(int ind) {
 		Lettre c = s.get().get(ind);
@@ -152,6 +180,7 @@ public class Modele {
 		
 		if(!en_cours) {
 			plateau[ind][jnd] = 1;
+			plat_char[ind][jnd] = c;
 			en_cours = true;
 			first_i=ind;
 			first_j=jnd;
@@ -159,22 +188,25 @@ public class Modele {
 		else {
 			if(ind == first_i || jnd == first_j) {
 				plateau[ind][jnd] = 1;
+				plat_char[ind][jnd] = c;
+				
 			}
 			else {
-				System.out.println("placer a coter de la dernière lettre poser");
+				System.out.println("placer a coter de la derniï¿½re lettre poser");
 			}
 		}
 		
-		if(!(first_turn())) {
-		if(verif_j1(c)) {
-			j1.remove(pivot);
-		}
-		vide_j1 +=1;
-		longeur +=1;
-		System.out.println(j1);
+		if(!(first_turn()) || plateau[ind][jnd] == 2) {
+			if(verif_j1(c)) {
+				mot_en_cours.add(j1.get(pivot));
+				j1.remove(pivot);
+			}
+			vide_j1 +=1;
+			longeur +=1;
+			System.out.println(j1);
 		}
 		else {
-			System.out.println("il faut jouer au milieu");
+			System.out.println("il faut jouer au milieu/jouer sur une lettre deja poser");
 		}
 		
 	}
@@ -189,7 +221,7 @@ public class Modele {
 				case 0 :
 					for(int i=first_j;i<first_j+longeur;i++) {
 						
-						if(plateau[first_i][i]!=1) {
+						if(plateau[first_i][i]!=1 && plateau[first_i][i]!=2 ) {
 							cas+=1;
 							break;
 						}
@@ -203,7 +235,7 @@ public class Modele {
 				case 1 :
 					for(int i=first_j;i>first_j-longeur;i--) {
 						
-						if(plateau[first_i][i]!=1) {
+						if(plateau[first_i][i]!=1 && plateau[first_i][i]!=2) {
 							cas+=1;
 							break;
 						}
@@ -216,7 +248,7 @@ public class Modele {
 					
 				case 2 : 
 					for(int i=first_i;i<first_i+longeur;i++) {
-						if(plateau[i][first_j]!=1) {
+						if(plateau[i][first_j]!=1 && plateau[i][first_j]!=2) {
 							cas+=1;
 							break;
 						}
@@ -230,7 +262,7 @@ public class Modele {
 				case 3 :
 					for(int i=first_i;i>first_i-longeur;i--) {
 						
-						if(plateau[i][first_j]!=1) {
+						if(plateau[i][first_j]!=1 && plateau[i][first_j]!=2) {
 							cas+=1;
 							break;
 						}
@@ -262,10 +294,33 @@ public class Modele {
 			first_j = 0;
 		}
 		else {
-			System.out.println("mot mal disposé");
+			for(int i=0;i<mot_en_cours.size();i++){
+				j1.add(mot_en_cours.get(i));
+			}
+			mot_en_cours.clear();
+			
+			//reset();
+			System.out.println("mot mal disposï¿½");
 		}
+		System.out.println(j1);
 	}
 	
+	public void reset(){
+		for(int i=0;i<plateau.length;i++){
+			for(int j=0;j<plateau.length;j++){
+				if(plateau[i][j]==1){
+					plateau[i][j] = mod_plateau[i][j];
+					plat_char[i][j] = '/';
+				}
+			}
+		}
+	}
+
+	public void resetAll(){
+		plateau = mod_plateau;
+
+	}
+
 	public void pioche() {
 		for(int i=0;i<vide_j1;i++) {
 			int out = r.nextInt(pieces.size());
@@ -279,13 +334,20 @@ public class Modele {
 	public static void main(String[] args) {
 		Modele m = new Modele();
 		m.first_tirage();
-		m.lettre_poser('i',7,7);
-		m.lettre_poser('j',7, 6);
-		m.lettre_poser('k', 7, 5);
-		m.lettre_poser('k', 7, 4);
-		
+		m.lettre_poser('p', 7, 7);
+		m.lettre_poser('x', 7, 6);
+		m.lettre_poser('d', 7, 5);
+		m.lettre_poser('n', 7, 4);
 		m.mot_fini();
+		m.pioche();
+		m.lettre_poser('j', 1, 1);
+		m.lettre_poser('z', 1, 2);
+		m.lettre_poser('i', 1, 3);
+		m.mot_fini();
+
 		m.print_plateau();
+		System.out.println();
+		m.print_charplat();
 		
 		
 		}
