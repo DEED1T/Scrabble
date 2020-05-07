d1afd314c20afd3c34f6a7d8938d3a7ff7f2ae03
Bastien Pic <bastienpic.bp@mail.com>
2020-05-07 13:26:38 +0200

    affichage joueur

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,37 +1,12 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-
-public class Plateau extends Application{
-	
-	
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
-		Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 400, 300));
-		primaryStage.show();
-	}
-	
-	
-	
-	
-	
+public class Plateau{
 	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
+		Application.launch(Test_affichage.class, args); //Lancement de Test_affichage (le plateau)
 		
 	}
-
 	
-
 }
