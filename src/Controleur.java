323e5a617cd53159aa0f311f943c3ae2aee5cdf2
Bastien Pic <bastienpic.bp@mail.com>
2020-05-10 17:33:48 +0200

    controfail2, disparition caractère séparateur

--- src/Controleur.java
+++ src/Controleur.java
@@ -131,12 +131,14 @@ public class Controleur {
 						
 						// Cette fonction est utilisée pour actualisé la main du joueur après avoir placé une lettre
 						private void actu(int ligne, int colonne) {
-							tabsLettres[ligne][colonne] = ' ';
+							tabsLettres[ligne][colonne] = '9';
+							printTabs(tabsLettres);
 							spaceLast(tabsLettres[ligne]);
-							
+							printTabs(tabsLettres);
 							
 							for(int col = 0; col < 7; col++) {
-								if(tabsLettres[ligne][col] != ' ') {
+								//if(tabsLettres[ligne][col] != '9') {
+								if(verifInteg(tabsLettres[ligne][colonne])) {
 									System.out.println("la lettre que l'on affiche est:"+tabsLettres[ligne][col]);
 									//afficher les lettres
 									ImageView newLettre;
@@ -163,19 +165,41 @@ public class Controleur {
 							
 						}
 						
+						private boolean verifInteg(char c) {
+							for(int i=0; i<Vue.alphabet.length; i++) {
+								System.out.println(c+" == "+Vue.alphabet[i]+" ?");
+								if(c == Vue.alphabet[i]) {
+									return true;
+								}
+							}
+							
+							return c == '*';
+						}
+
+						private void printTabs(char[][] tabsLettres) {
+							for(int i=0; i<tabsLettres.length; i++) {
+								System.out.print("[");
+								for(int j=0; j<tabsLettres[i].length; j++) {
+									System.out.print(tabsLettres[i][j]);
+								}
+								System.out.println("]");
+							}
+							
+						}
+
 						// Cette fonction permet de placer null à la fin du tableau -> avoir les lettres au début
 						private void spaceLast(char[] lettres) {
 							boolean encore = true;
 							int i = 0;
 							while(encore){
-								if(lettres[i] == ' ') {
+								if(lettres[i] == '9') {
 									if(i == lettres.length-1) {
 										encore = false;
 									}
 									else {
-										if(lettres[i+1] != ' ') {
-										lettres[i] = lettres[i+1];
-										lettres[i+1] = ' ';
+										if(lettres[i+1] != '9') {
+											lettres[i] = lettres[i+1];
+											lettres[i+1] = '9';
 										}
 										else {
 											encore = false;
