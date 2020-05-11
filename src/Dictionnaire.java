6eaec1070edcaf1f9c8f6578854e3f52e1b390b3
Lazaar Hamdi <hamdilazaar.vp@gmail.com>
2020-05-11 16:33:50 +0200

    tentaticve de serialisation

--- src/Dictionnaire.java
+++ src/Dictionnaire.java
@@ -1,10 +1,16 @@
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.TreeSet;
 
 public class Dictionnaire<E> extends TreeSet<String> {
+	
+	public File fichierout = new File("dico.xml");
 	
 	public Dictionnaire()  {
 		super();
@@ -19,11 +25,16 @@ public class Dictionnaire<E> extends TreeSet<String> {
 			while(((lignebr1 = br1.readLine()) != null)){
 				lignebr2 = br2.readLine();
 				this.add(lignebr1);
+				//enregistrer(lignebr1);
 				if(lignebr2 != null) {
 					this.add(lignebr2);
+					//enregistrer(lignebr2);
 				}
 				
 			}
+			
+			br1.close();
+			br2.close();
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -31,6 +42,26 @@ public class Dictionnaire<E> extends TreeSet<String> {
 		}
 		
 		}
+	
+	public void enregistrer(String s) {
+		XMLEncoder encoder = null;
+		
+		try {
+			FileOutputStream f_out = new FileOutputStream(fichierout);
+			BufferedOutputStream b_out = new BufferedOutputStream(f_out);
+			encoder = new XMLEncoder(b_out);
+			encoder.writeObject(s);
+			encoder.flush();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(encoder != null) {
+			encoder.close();
+		}
+		
+	}
 	
 	
 	public static void main(String[] args) {
